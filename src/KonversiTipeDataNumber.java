6844c5bbeb0034f871ab8bbdac5ae667f639dc4b
muhammadkusuma <mwiraadekusuma1@gmail.com>
2025-11-06 16:44:02 +0700

    Tipe Data Number

---
+++ src/KonversiTipeDataNumber.java
@@ -0,0 +1,10 @@
+public class KonversiTipeDataNumber {
+    static void main() {
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+
+        byte iniByte2= (byte) iniInt;
+
+    }
+}
---
+++ src/TipeDataNumber.java
@@ -0,0 +1,21 @@
+public class TipeDataNumber {
+    static void main() {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInteger = 10000000;
+        long iniLong = 100000000;
+        long iniLong2 = 100000000L;
+
+        float iniFloat = 10.10F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 25;
+        int hexInt = 0xA132B;
+        int binInt = 0b01010101;
+
+        long balance = 1_000_000_000_000L;
+        int sum = 60_000_000;
+
+
+    }
+}
